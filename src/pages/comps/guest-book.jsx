d445f8a1a614d094c1cfe836bc2a22fd07767ccb
leejy <lastiverse@gmail.com>
2015-11-23 22:19:49 +0900

    -

--- src/pages/comps/guest-book.jsx
+++ src/pages/comps/guest-book.jsx
@@ -1,6 +1,11 @@
 var React = require('react');
 var PolymerIcon = require('../../comps/polymer-icon.jsx');
 var Button = require('material-ui/lib/raised-button');
+var TextField = require('material-ui/lib/text-field');
+var Card = require('material-ui/lib/card/card');
+var CardHeader = require('material-ui/lib/card/card-header');
+var CardText = require('material-ui/lib/card/card-text');
+var CardTitle = require('material-ui/lib/card/card-title');
 require('./guest-book.less');
 
 var GuestBookPage = React.createClass({
@@ -14,10 +19,24 @@ var GuestBookPage = React.createClass({
 
 	render() {
 		return (
-			<div
-				className="guest-book-container"
+			<div className="guest-book-container"
 				style={{ display: this.props.visible === true ? 'block' : 'none' }}>
-				<Button label="글 남기기" />
+				<Card>
+					<CardText>
+						<TextField
+							floatingLabelText="방명록"
+							multiLine={true}
+							style={{ width: '100%' }}
+							underlineFocusStyle={{ borderColor: 'gray' }}
+							floatingLabelStyle={{ color: 'gray' }} />
+						<div className="btn-area">
+							<Button 
+								label="남기기" 
+								primary={true}
+								backgroundColor="gray" />
+						</div>
+					</CardText>
+				</Card>
 			</div>
 		);
 	}
--- src/pages/comps/map.jsx
+++ src/pages/comps/map.jsx
@@ -1,4 +1,5 @@
 var React = require('react');
+var util = require('util');
 var PolymerIcon = require('../../comps/polymer-icon.jsx');
 require('./map.less');
 
@@ -14,9 +15,10 @@ var MapPage = React.createClass({
 	getDaumMapMarkup() {
 		return {
 			__html: 
-				'<div id="daumRoughmapContainer1448251629479" className="root_daum_roughmap root_daum_roughmap_landing"></div> \
+				'<div id="daumRoughmapContainer1448251629479" class="root_daum_roughmap root_daum_roughmap_landing"></div> \
 				<script charset="UTF-8" class="daum_roughmap_loader_script" src="http://dmaps.daum.net/map_js_init/roughmapLoader.js"></script> \
 				<script charset="UTF-8"> \
+					alert("a"); \
 					new daum.roughmap.Lander({ \
 						"timestamp" : "1448251629479", \
 						"key" : "6v47", \
@@ -27,11 +29,38 @@ var MapPage = React.createClass({
 		};
 	},
 
+	getNaverMapURL() {
+		var params = '';
+		params += 'version=1.0';
+		params += '&exception=inimage';
+		params += '&center=127.927813,36.994482';
+		params += '&level=10';
+		params += '&w=300';
+		params += '&h=400';
+		params += '&baselayer=default';
+		params += '&key=f41a794e63f830234f725e213f2e4e3b';
+		params += '&uri=localhost';
+		params += '&markers=127.927813,36.994482';
+
+		return 'http://openapi.naver.com/map/getStaticMap?' + params;
+	},
+
+	goToNaverMap() {
+		window.location.href = 'http://map.naver.com/?dlevel=12&pinType=site&pinId=35585757&x=127.9279455&y=36.9945793&enc=b64';
+	},
+
 	render() {
 		return (
-			<div className="map-container"
-				style={{ display: this.props.visible === true ? 'block' : 'none' }}
-				dangerouslySetInnerHTML={this.getDaumMapMarkup()} />
+			<div 
+				className="map-container"
+				style={{ display: this.props.visible === true ? 'block' : 'none' }}>
+				<img 
+					className="map-img" 
+					src={this.getNaverMapURL()} />
+				<a href="http://map.naver.com/?dlevel=12&pinType=site&pinId=35585757&x=127.9279455&y=36.9945793&enc=b64">
+					네이버 지도에서 보기 
+				</a>
+			</div>
 		);
 	}
 });
