e956b0e7c6b915239aa1307e00c2633b8b9a60dc
leejy <lastiverse@gmail.com>
2015-11-29 18:29:44 +0900

    -

--- public/bundle.js
+++ public/bundle.js
@@ -21071,8 +21071,16 @@
 	'use strict';
 
 	var React = __webpack_require__(1);
+	var LazyLoad = __webpack_require__(353).default;
 	var PolymerIcon = __webpack_require__(159);
 	__webpack_require__(173);
+
+	console.log('test');
+	console.log(LazyLoad);
+	console.log(PolymerIcon);
+	console.log('test1');
+
+	var photoUrls = ['https://googledrive.com/host/0B0Q0WceCbJRzbFVpUnFlNDR5UEE/HONG0004.jpg', 'https://googledrive.com/host/0B0Q0WceCbJRzbFVpUnFlNDR5UEE/HONG0018.jpg', 'https://googledrive.com/host/0B0Q0WceCbJRzbFVpUnFlNDR5UEE/HONG0035.jpg', 'https://googledrive.com/host/0B0Q0WceCbJRzbFVpUnFlNDR5UEE/HONG0064.jpg', 'https://googledrive.com/host/0B0Q0WceCbJRzbFVpUnFlNDR5UEE/HONG0106.jpg', 'https://googledrive.com/host/0B0Q0WceCbJRzbFVpUnFlNDR5UEE/HONG0220.jpg', 'https://googledrive.com/host/0B0Q0WceCbJRzbFVpUnFlNDR5UEE/HONG0409.jpg', 'https://googledrive.com/host/0B0Q0WceCbJRzbFVpUnFlNDR5UEE/HONG0592.jpg', 'https://googledrive.com/host/0B0Q0WceCbJRzbFVpUnFlNDR5UEE/HONG0701.jpg', 'https://googledrive.com/host/0B0Q0WceCbJRzbFVpUnFlNDR5UEE/HONG0819.jpg', 'https://googledrive.com/host/0B0Q0WceCbJRzbFVpUnFlNDR5UEE/HONG0832.jpg', 'https://googledrive.com/host/0B0Q0WceCbJRzbFVpUnFlNDR5UEE/HONG0948.jpg', 'https://googledrive.com/host/0B0Q0WceCbJRzbFVpUnFlNDR5UEE/HONG0975.jpg', 'https://googledrive.com/host/0B0Q0WceCbJRzbFVpUnFlNDR5UEE/HONG1038.jpg', 'https://googledrive.com/host/0B0Q0WceCbJRzbFVpUnFlNDR5UEE/HONG1142.jpg', 'https://googledrive.com/host/0B0Q0WceCbJRzbFVpUnFlNDR5UEE/HONG1244.jpg', 'https://googledrive.com/host/0B0Q0WceCbJRzbFVpUnFlNDR5UEE/HONG1281.jpg', 'https://googledrive.com/host/0B0Q0WceCbJRzbFVpUnFlNDR5UEE/HONG1313.jpg', 'https://googledrive.com/host/0B0Q0WceCbJRzbFVpUnFlNDR5UEE/HONG1360.jpg', 'https://googledrive.com/host/0B0Q0WceCbJRzbFVpUnFlNDR5UEE/HONG1400.jpg', 'https://googledrive.com/host/0B0Q0WceCbJRzbFVpUnFlNDR5UEE/HONG1443.jpg', 'https://googledrive.com/host/0B0Q0WceCbJRzbFVpUnFlNDR5UEE/HONG1483.jpg'];
 
 	var PhotoPage = React.createClass({
 		displayName: 'PhotoPage',
@@ -21090,7 +21098,13 @@
 				{
 					className: 'photo-container',
 					style: { display: this.props.visible === true ? 'block' : 'none' } },
-				'photo'
+				photoUrls.map(function (url) {
+					return React.createElement(
+						LazyLoad,
+						{ threshold: 400, key: url },
+						React.createElement('img', { className: 'photo', src: url })
+					);
+				})
 			);
 		}
 	});
@@ -21134,7 +21148,7 @@
 
 
 	// module
-	exports.push([module.id, "", ""]);
+	exports.push([module.id, ".photo-container img.photo {\n  width: 100%;\n}\n", ""]);
 
 	// exports
 
@@ -41726,6 +41740,177 @@
 	    return zh_tw;
 
 	}));
+
+/***/ },
+/* 353 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+
+	var _react = __webpack_require__(1);
+
+	var _react2 = _interopRequireDefault(_react);
+
+	var _reactDom = __webpack_require__(158);
+
+	var _classnames = __webpack_require__(354);
+
+	var _classnames2 = _interopRequireDefault(_classnames);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+	var LazyLoad = (function (_Component) {
+	  _inherits(LazyLoad, _Component);
+
+	  function LazyLoad(props) {
+	    _classCallCheck(this, LazyLoad);
+
+	    var _this = _possibleConstructorReturn(this, Object.getPrototypeOf(LazyLoad).call(this, props));
+
+	    _this.state = {
+	      visible: false
+	    };
+
+	    _this.onWindowScroll = _this.onWindowScroll.bind(_this);
+	    return _this;
+	  }
+
+	  _createClass(LazyLoad, [{
+	    key: 'componentDidMount',
+	    value: function componentDidMount() {
+	      window.addEventListener('scroll', this.onWindowScroll);
+	      window.addEventListener('resize', this.onWindowScroll);
+	      this.onWindowScroll();
+	    }
+	  }, {
+	    key: 'componentDidUpdate',
+	    value: function componentDidUpdate() {
+	      if (!this.state.visible) this.onWindowScroll();
+	    }
+	  }, {
+	    key: 'componentWillUnmount',
+	    value: function componentWillUnmount() {
+	      this.onVisible();
+	    }
+	  }, {
+	    key: 'onVisible',
+	    value: function onVisible() {
+	      window.removeEventListener('scroll', this.onWindowScroll);
+	      window.removeEventListener('resize', this.onWindowScroll);
+	    }
+	  }, {
+	    key: 'onWindowScroll',
+	    value: function onWindowScroll() {
+	      var threshold = this.props.threshold;
+
+	      var bounds = (0, _reactDom.findDOMNode)(this).getBoundingClientRect();
+	      var scrollTop = window.pageYOffset;
+	      var top = bounds.top + scrollTop;
+	      var height = bounds.bottom - bounds.top;
+
+	      if (top === 0 || top <= scrollTop + window.innerHeight + threshold && top + height > scrollTop - threshold) {
+	        this.setState({ visible: true });
+	        this.onVisible();
+	      }
+	    }
+	  }, {
+	    key: 'render',
+	    value: function render() {
+	      var elStyles = {
+	        height: this.props.height
+	      };
+	      var elClasses = (0, _classnames2.default)({
+	        'lazy-load': true,
+	        'lazy-load-visible': this.state.visible
+	      });
+
+	      return _react2.default.createElement(
+	        'div',
+	        { className: elClasses, style: elStyles },
+	        this.state.visible && this.props.children
+	      );
+	    }
+	  }]);
+
+	  return LazyLoad;
+	})(_react.Component);
+
+	exports.default = LazyLoad;
+
+	LazyLoad.propTypes = {
+	  children: _react.PropTypes.node.isRequired,
+	  height: _react.PropTypes.oneOfType([_react.PropTypes.string, _react.PropTypes.number]),
+	  threshold: _react.PropTypes.number
+	};
+	LazyLoad.defaultProps = {
+	  threshold: 0
+	};
+
+/***/ },
+/* 354 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;/*!
+	  Copyright (c) 2015 Jed Watson.
+	  Licensed under the MIT License (MIT), see
+	  http://jedwatson.github.io/classnames
+	*/
+	/* global define */
+
+	(function () {
+		'use strict';
+
+		var hasOwn = {}.hasOwnProperty;
+
+		function classNames () {
+			var classes = '';
+
+			for (var i = 0; i < arguments.length; i++) {
+				var arg = arguments[i];
+				if (!arg) continue;
+
+				var argType = typeof arg;
+
+				if (argType === 'string' || argType === 'number') {
+					classes += ' ' + arg;
+				} else if (Array.isArray(arg)) {
+					classes += ' ' + classNames.apply(null, arg);
+				} else if (argType === 'object') {
+					for (var key in arg) {
+						if (hasOwn.call(arg, key) && arg[key]) {
+							classes += ' ' + key;
+						}
+					}
+				}
+			}
+
+			return classes.substr(1);
+		}
+
+		if (typeof module !== 'undefined' && module.exports) {
+			module.exports = classNames;
+		} else if (true) {
+			// register as 'classnames', consistent with npm package name
+			!(__WEBPACK_AMD_DEFINE_ARRAY__ = [], __WEBPACK_AMD_DEFINE_RESULT__ = function () {
+				return classNames;
+			}.apply(exports, __WEBPACK_AMD_DEFINE_ARRAY__), __WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__));
+		} else {
+			window.classNames = classNames;
+		}
+	}());
+
 
 /***/ }
 /******/ ]);
--- src/pages/comps/photo.jsx
+++ src/pages/comps/photo.jsx
@@ -1,6 +1,37 @@
 var React = require('react');
+var LazyLoad = require('react-lazy-load').default;
 var PolymerIcon = require('../../comps/polymer-icon.jsx');
 require('./photo.less');
+
+console.log('test');
+console.log(LazyLoad);
+console.log(PolymerIcon);
+console.log('test1');
+
+var photoUrls = [
+	'https://googledrive.com/host/0B0Q0WceCbJRzbFVpUnFlNDR5UEE/HONG0004.jpg',
+	'https://googledrive.com/host/0B0Q0WceCbJRzbFVpUnFlNDR5UEE/HONG0018.jpg',
+	'https://googledrive.com/host/0B0Q0WceCbJRzbFVpUnFlNDR5UEE/HONG0035.jpg',
+	'https://googledrive.com/host/0B0Q0WceCbJRzbFVpUnFlNDR5UEE/HONG0064.jpg',
+	'https://googledrive.com/host/0B0Q0WceCbJRzbFVpUnFlNDR5UEE/HONG0106.jpg',
+	'https://googledrive.com/host/0B0Q0WceCbJRzbFVpUnFlNDR5UEE/HONG0220.jpg',
+	'https://googledrive.com/host/0B0Q0WceCbJRzbFVpUnFlNDR5UEE/HONG0409.jpg',
+	'https://googledrive.com/host/0B0Q0WceCbJRzbFVpUnFlNDR5UEE/HONG0592.jpg',
+	'https://googledrive.com/host/0B0Q0WceCbJRzbFVpUnFlNDR5UEE/HONG0701.jpg',
+	'https://googledrive.com/host/0B0Q0WceCbJRzbFVpUnFlNDR5UEE/HONG0819.jpg',
+	'https://googledrive.com/host/0B0Q0WceCbJRzbFVpUnFlNDR5UEE/HONG0832.jpg',
+	'https://googledrive.com/host/0B0Q0WceCbJRzbFVpUnFlNDR5UEE/HONG0948.jpg',
+	'https://googledrive.com/host/0B0Q0WceCbJRzbFVpUnFlNDR5UEE/HONG0975.jpg',
+	'https://googledrive.com/host/0B0Q0WceCbJRzbFVpUnFlNDR5UEE/HONG1038.jpg',
+	'https://googledrive.com/host/0B0Q0WceCbJRzbFVpUnFlNDR5UEE/HONG1142.jpg',
+	'https://googledrive.com/host/0B0Q0WceCbJRzbFVpUnFlNDR5UEE/HONG1244.jpg',
+	'https://googledrive.com/host/0B0Q0WceCbJRzbFVpUnFlNDR5UEE/HONG1281.jpg',
+	'https://googledrive.com/host/0B0Q0WceCbJRzbFVpUnFlNDR5UEE/HONG1313.jpg',
+	'https://googledrive.com/host/0B0Q0WceCbJRzbFVpUnFlNDR5UEE/HONG1360.jpg',
+	'https://googledrive.com/host/0B0Q0WceCbJRzbFVpUnFlNDR5UEE/HONG1400.jpg',
+	'https://googledrive.com/host/0B0Q0WceCbJRzbFVpUnFlNDR5UEE/HONG1443.jpg',
+	'https://googledrive.com/host/0B0Q0WceCbJRzbFVpUnFlNDR5UEE/HONG1483.jpg'
+];
 
 var PhotoPage = React.createClass({
 	PropTypes: {
@@ -16,7 +47,15 @@ var PhotoPage = React.createClass({
 			<div
 				className="photo-container"
 				style={{ display: this.props.visible === true ? 'block' : 'none'}}>
-				photo
+				{
+					photoUrls.map(function(url) {
+						return ( 
+							<LazyLoad threshold={400} key={url}>
+								<img className="photo" src={url} /> 
+							</LazyLoad>
+						);
+					})
+				}
 			</div>
 		);
 	}
